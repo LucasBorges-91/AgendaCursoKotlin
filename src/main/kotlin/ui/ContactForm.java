c0cb9ddaf40f40b04699967f94e9cf83cf5ba037
Lucas Borges <lucas.pborges@yahoo.com.br>
2021-12-31 14:51:33 -0300

    salvando contatos

--- src/main/kotlin/ui/ContactForm.java
+++ src/main/kotlin/ui/ContactForm.java
@@ -1,5 +1,7 @@
 package ui;
 
+import business.ContactBusiness;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -13,6 +15,8 @@ public class ContactForm extends JFrame {
   private JButton buttonCancel;
   private JButton buttonSave;
 
+  private ContactBusiness mContactBusiness;
+
   public ContactForm() {
 
     setContentPane(rootPanel);
@@ -23,6 +27,8 @@ public class ContactForm extends JFrame {
     setLocation(dim.width / 2 - getSize().width / 2, dim.height / 2 - getSize().height / 2);
 
     setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+
+    mContactBusiness = new ContactBusiness();
 
     setListeners();
   }
@@ -39,8 +45,18 @@ public class ContactForm extends JFrame {
     buttonSave.addActionListener(new ActionListener() {
       @Override
       public void actionPerformed(ActionEvent e) {
+        try {
+          String name = textName.getText();
+          String phone = textPhone.getText();
 
-      }
+          mContactBusiness.saveContact( name, phone );
+
+          new MainForm();
+          dispose();
+        } catch (Exception ex) {
+          JOptionPane.showMessageDialog(new JFrame(), ex.getMessage());
+        }
+        }
     });
   }
 }
